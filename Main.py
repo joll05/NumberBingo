61d403aebba3d8e6927f56965faeb84aaf1edb43
joll05 <44553291+joll05@users.noreply.github.com>
2020-01-31 20:54:21 +0100

    Started on some basic functions

--- Main.py
+++ Main.py
@@ -1,1 +1,12 @@
+from sys import argv
 from PyQt5.QtWidgets import *
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(QMainWindow, self).__init__()
+        self.setWindowTitle("Hello world!")
+
+app = QApplication(argv)
+window = MainWindow()
+window.show()
+app.exec_()
